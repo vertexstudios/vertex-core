34304203fc69def50987cc1e0139002b96e7e552
threader <40972803+localthreader@users.noreply.github.com>
2021-07-29 09:23:42 -0300

    Adding database acessor abstract class and sqlite acessor

---
+++ src/main/java/net/threader/lib/sql/DatabaseAcessor.java
@@ -0,0 +1,29 @@
+package net.threader.lib.sql;
+
+import org.bukkit.plugin.Plugin;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class DatabaseAcessor {
+    protected Connection connection;
+    protected Plugin plugin;
+    protected String url;
+
+    public DatabaseAcessor(String url) {
+        this.url = url;
+    }
+
+    public Connection getConnection() {
+        try {
+            if(connection == null || connection.isClosed()) {
+                connect();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return connection;
+    }
+
+    public abstract void connect();
+}

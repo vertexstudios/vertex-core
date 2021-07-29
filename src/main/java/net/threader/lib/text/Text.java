c14f4f8584f905d290167eb0407072550b1e1a3f
threader <40972803+localthreader@users.noreply.github.com>
2021-07-29 15:22:43 -0300

    Implementing variables in text api

--- src/main/java/net/threader/lib/text/Text.java
+++ src/main/java/net/threader/lib/text/Text.java
@@ -7,18 +7,54 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Text implements IText {
     private List<Object> objects;
+    private Map<String, Object> variables;
 
-    public Text(List<Object> objects) {
-        this.objects = objects;
+    public Text(List<Object> objects, Map<String, Object> variables) {
+        this.variables = variables;
+        this.objects = this.translateVariables(objects);
     }
 
     @Override
     public List<Object> getObjects() {
         return objects;
+    }
+
+    private List<Object> translateVariables(List<Object> objects) {
+        List<Object> translated = new ArrayList<>();
+        objects.stream().forEach(object -> {
+            if(object instanceof String) {
+                String currentText = (String) object;
+                if(currentText.contains("${")) {
+                    char[] charSequence = currentText.toCharArray();
+                    int firstIndex = currentText.indexOf("${") + 2;
+                    if(currentText.substring(firstIndex).contains("}")) {
+                        StringBuilder var = new StringBuilder();
+                        for(int i = firstIndex; i < charSequence.length; i++) {
+                            if(charSequence[i] == '}') {
+                                if (variables.containsKey(var.toString())) {
+                                    currentText = currentText.replace("${" + var + "}", variables.get(var.toString()).toString());
+                                    break;
+                                }
+                            }
+                            var.append(charSequence[i]);
+                        }
+                    }
+                }
+            } else {
+                translated.add(object);
+            }
+        });
+        return translated;
     }
 
     @Override
@@ -52,7 +88,8 @@ public class Text implements IText {
     }
 
     public static class Builder {
-        private List<Object> objects;
+        private List<Object> objects = new ArrayList<>();
+        private Map<String, Object> variables = new HashMap<>();
 
         public Builder color(String hex) {
             objects.add(new HexColor(hex));
@@ -74,8 +111,13 @@ public class Text implements IText {
             return this;
         }
 
+        public Builder variable(String id, Object value) {
+            variables.put(id, value);
+            return this;
+        }
+
         public Text build() {
-            return new Text(objects);
+            return new Text(objects, variables);
         }
 
     }

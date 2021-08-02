56d6c41e5f5613a73f801f693f541e30bba1f12d
threader <40972803+localthreader@users.noreply.github.com>
2021-08-01 22:10:05 -0300

    Merging ObjectRetrievePath from magical items and fixing incompatible types in ItemStackBuilder

--- src/main/java/net/threader/lib/util/ItemStackBuilder.java
+++ src/main/java/net/threader/lib/util/ItemStackBuilder.java
@@ -1,14 +1,18 @@
 package net.threader.lib.util;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ItemStackBuilder {
@@ -16,6 +20,8 @@ public class ItemStackBuilder {
     private Material material = Material.APPLE;
     private List<String> lore = new ArrayList<>();
     private Map<Enchantment, Integer> enchants = new HashMap<>();
+    private Set<Triple<NamespacedKey, PersistentDataType<?,?>, Object>> persistentData = new HashSet<>();
+
     private int amount = 1;
 
     public static ItemStackBuilder factory() {
@@ -52,6 +58,11 @@ public class ItemStackBuilder {
         return this;
     }
 
+    public ItemStackBuilder persistentData(NamespacedKey key, PersistentDataType<?,?> type, Object value) {
+        this.persistentData.add(new Triple<>(key, type, value));
+        return this;
+    }
+
     public ItemStack build() {
         ItemStack stack = new ItemStack(material, amount);
         stack.addUnsafeEnchantments(enchants);
@@ -59,6 +70,9 @@ public class ItemStackBuilder {
         if(title != null) {
             meta.setDisplayName(title.replace("&", "§"));
         }
+        persistentData.forEach(x -> {
+            meta.getPersistentDataContainer().set(x.getFirst(), (PersistentDataType) x.getSecond(), x.getThird());
+        });
         meta.setLore(lore.stream().map(lore -> lore.replace("&", "§")).collect(Collectors.toList()));
         stack.setItemMeta(meta);
         return stack;

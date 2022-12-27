019c14074511b254c1a25f30e626882d1acc05d1
Victor Betoni <victorbetoni@protonmail.com>
2022-12-26 21:45:57 -0300

    Using buffered reader

--- src/main/java/net/threader/lib/sql/ScriptExecutor.java
+++ src/main/java/net/threader/lib/sql/ScriptExecutor.java
@@ -2,13 +2,7 @@ package net.threader.lib.sql;
 
 import net.threader.lib.sql.acessor.DatabaseAcessor;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,32 +10,32 @@ import java.util.Arrays;
 
 public class ScriptExecutor {
 
-    private File file;
+    private BufferedReader reader;
     private Connection connection;
 
-    public ScriptExecutor(File file, Connection connection) {
-        this.file = file;
+    public ScriptExecutor(BufferedReader reader, Connection connection) {
+        this.reader = reader;
         this.connection = connection;
     }
 
     public void execute() {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))){
+        try {
             StringBuilder fileContent = new StringBuilder();
             String line = "";
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 fileContent.append(line);
             }
             final String[] queries = fileContent.toString().split(";");
             Arrays.stream(queries).forEach((query) -> {
-                try{
+                try {
                     Statement st = connection.createStatement();
                     st.execute(query);
-                }catch (SQLException ex){
+                } catch (SQLException ex) {
                     ex.printStackTrace();
                 }
             });
-        }catch (IOException ex){
-            ex.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 

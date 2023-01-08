f0e51d29e65a8287de196e0ba68c21f68294d9b2
Victor Betoni <victorbetoni@protonmail.com>
2023-01-08 16:36:26 -0300

    Refactoring Text API

--- src/main/java/net/threadly/core/text/Text.java
+++ src/main/java/net/threadly/core/text/Text.java
@@ -1,144 +1,84 @@
 package net.threadly.core.text;
 
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.threadly.core.text.color.HexColor;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
-public class Text implements IText {
-    private List<Object> objects;
-    private Map<String, Object> variables = new HashMap<>();
+public class Text {
+    protected Map<String, Object> placeholders = new HashMap<>();
+    protected List<String> lines = new ArrayList<>();
 
-    public Text(List<Object> objects) {
-        this.objects = this.translateVariables(objects);
+    public Text(Map<String, Object> placeholders, List<String> lines) {
+        this.placeholders = placeholders;
+        this.lines = lines;
     }
 
-    public Text addVariable(String id, Object value) {
-        variables.put(id, value);
-        return this;
+    public Text() {
     }
 
-    public static Text of(String text) {
-        char[] chars = text.toCharArray();
-        Builder builder = builder();
-        for(int i = 0; i < chars.length; i++) {
-            StringBuilder currentMessage = new StringBuilder();
-            if(i != 0 && chars[i - 1] == '&') {
-                continue;
-            }
-            if(chars[i] == '&' && !(i == chars.length - 1)) {
-                builder.message(currentMessage.toString());
-                currentMessage = new StringBuilder();
-                char code = chars[i + 1];
-                if(code == '#') {
-                    StringBuilder hex = new StringBuilder();
-                    if(chars.length - (i + 1) >= 8) {
-                        for(int j = 1; j<7; j++) {
-                            hex.append(chars[i + j]);
-                        }
-                        builder.color(hex.toString());
-                        i += 7;
-                    }
-                } else if (Style.byCode(code).isPresent()) {
-                    builder.style(Style.byCode(code).get());
-                } else if (ChatColor.getByChar(code) != null) {
-                    builder.color(ChatColor.getByChar(code));
-                } else {
-                    currentMessage.append(chars[i]);
-                }
-            } else {
-                currentMessage.append(chars[i]);
-            }
-        }
-        return builder.build();
+    public void colorizeLines() {
+        lines = lines.stream().map(line -> ChatColor.translateAlternateColorCodes('&', line)).collect(Collectors.toList());
     }
 
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    @Override
-    public List<Object> getObjects() {
-        return objects;
-    }
-
-    private List<Object> translateVariables(List<Object> objects) {
-        List<Object> translated = new ArrayList<>();
-        objects.forEach(object -> {
-            if(object instanceof String) {
-                AtomicReference<String> currentText = new AtomicReference<>((String) object);
-                variables.forEach((id, value) -> currentText.set(currentText.get().replace("${" + id + "}", value.toString())));
-            } else {
-                translated.add(object);
+    public String replaceVariables(String line) {
+        AtomicReference<String> currentLine = new AtomicReference<>(line);
+        placeholders.forEach((key, value) -> {
+            if (line.contains("{" + key + "}")) {
+                currentLine.set(currentLine.get().replace("{" + key + "}", value.toString()));
             }
         });
-        return translated;
+        return currentLine.get();
     }
 
-    @Override
-    public BaseComponent[] build() {
-        ComponentBuilder builder = new ComponentBuilder();
-        objects.forEach(obj -> {
-            if(obj instanceof String) {
-                builder.append((String) obj);
-            }
-            if(obj instanceof Style) {
-                Style style = (Style) obj;
-                try {
-                    builder.getClass().getDeclaredMethod(style.getMethod()).invoke(builder, Boolean.TRUE);
-                } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(obj instanceof ChatColor) {
-                builder.color(((ChatColor) obj).asBungee());
-            }
-            if(obj instanceof HexColor) {
-                builder.color(net.md_5.bungee.api.ChatColor.of(((HexColor) obj).getHex()));
-            }
-        });
-        return builder.create();
-    }
-
-    @Override
     public void send(Player player) {
-        player.spigot().sendMessage(this.build());
+        colorizeLines();
+        lines.forEach(line -> {
+            player.sendMessage(replaceVariables(line));
+        });
     }
 
     public static class Builder {
-        private List<Object> objects = new ArrayList<>();
+        List<String> lines = new ArrayList<>();
+        private Map<String, Object> placeholders = new HashMap<>();
 
-        public Builder color(String hex) {
-            objects.add(new HexColor(hex));
+        public Builder addLine(String line) {
+            lines.add(line);
             return this;
         }
 
-        public Builder color(ChatColor color) {
-            objects.add(color);
+        public Builder addLines(String... lines) {
+            this.lines.addAll(Arrays.asList(lines));
             return this;
         }
 
-        public Builder message(String message) {
-            objects.add(message);
+        public Builder addLines(List<String> lines) {
+            this.lines.addAll(lines);
             return this;
         }
 
-        public Builder style(Style style) {
-            objects.add(style);
+        public Builder fromConfig(String path, Plugin plugin) {
+            FileConfiguration config = plugin.getConfig();
+            path = "message." + path;
+            if(config.get(path) instanceof List) {
+                addLines(config.getStringList(path));
+            } else {
+                addLine(config.getString(path));
+            }
+            return this;
+        }
+
+        public Builder setPlaceholder(String var, Object value) {
+            placeholders.put(var, value);
             return this;
         }
 
         public Text build() {
-            return new Text(objects);
+            return new Text(placeholders, lines);
         }
-
     }
 }

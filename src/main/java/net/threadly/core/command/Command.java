aa06e52403b5a997171c0978333300c54abf0284
Victor Betoni <victorbetoni@protonmail.com>
2023-01-08 23:12:03 -0300

    Setting default empty array for command params

--- src/main/java/net/threadly/core/command/Command.java
+++ src/main/java/net/threadly/core/command/Command.java
@@ -4,13 +4,14 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Arrays;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Command {
     String usage();
 
-    CommandParam[] params();
+    CommandParam[] params() default {};
 
     String permission() default "";
 

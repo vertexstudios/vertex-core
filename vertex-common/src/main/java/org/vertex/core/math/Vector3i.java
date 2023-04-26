098a6556cd5b0bf8d82e9735cb8b078313d5f747
victorbetoni <victorbetoni@protonmail.com>
2023-04-26 15:42:02 -0300

    Adding math vector

---
+++ vertex-common/src/main/java/org/vertex/core/math/Vector3i.java
@@ -0,0 +1,13 @@
+package org.vertex.core.math;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class Vector3i {
+
+    @Getter private int x;
+    @Getter private int y;
+    @Getter private int z;
+
+}

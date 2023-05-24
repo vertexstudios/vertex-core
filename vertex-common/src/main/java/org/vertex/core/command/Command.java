d1eb8080e7a5c330ce2647d85afb881903133815
victorbetoni <victorbetoni@protonmail.com>
2023-05-24 18:10:12 -0300

    Fixing field

--- vertex-common/src/main/java/org/vertex/core/command/Command.java
+++ vertex-common/src/main/java/org/vertex/core/command/Command.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Command {
-    String value();
+    String usage();
 
     CommandParam[] params() default {};
 
